54ba603808226d993bb6e50ea82b847015432a29
James Hunt <37090809+James-J-Hunt@users.noreply.github.com>
2019-04-25 21:47:14 +1200

    Location Added

--- javascript.js
+++ javascript.js
@@ -1,7 +1,13 @@
 function init() {
+    //Compass Variables
     var compass = document.getElementById('compass');
-        if(window.DeviceOrientationEvent) {
 
+    //Position Variables
+    const status = document.querySelector('#status');
+    const mapLink = document.querySelector('#map-link');
+
+    //Compass Function
+    if(window.DeviceOrientationEvent) {
         window.addEventListener('deviceorientation', function(event) {
             var alpha;
             //Check for iOS property
@@ -21,9 +27,33 @@ function init() {
             }
 
             compass.style.Transform = 'rotate(' + alpha +'deg)';
-            compass.style.WebkitTransform = 'rotate('+ webkitAlpha-180 + 'deg)';
+            compass.style.WebkitTransform = 'rotate('+ webkitAlpha + 'deg)';
             //Rotation is reversed for FF
             compass.style.MozTransform = 'rotate(-' + alpha + 'deg)'; 
         }, false);
+    }
+
+    //Position Function
+    mapLink.href = '';
+    mapLink.textContent = '';
+
+    function success(position) {
+        const latitude  = position.coords.latitude;
+        const longitude = position.coords.longitude;
+
+        status.textContent = '';
+        mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+        mapLink.textContent = `Latitude: ${latitude}, Longitude: ${longitude}`;
+    }
+
+    function error() {
+        status.textContent = 'Unable to retrieve your location';
+    }
+
+    if (!navigator.geolocation) {
+        status.textContent = 'Geolocation is not supported by your browser';
+    } else {
+        status.textContent = 'Locating';
+        navigator.geolocation.getCurrentPosition(success, error);
     }
 } 
